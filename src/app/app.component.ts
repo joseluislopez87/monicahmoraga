fb02f4875e4ba52cb1c7bf9f25e46c433d6739e1
José Luis López <joseluislopez87@gmail.com>
2019-06-26 20:22:19 +0200

    Setup Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 import { fadeAnimation } from "./animations";
+import { Router, NavigationEnd } from "@angular/router";
+import {filter} from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: "app-root",
@@ -7,4 +11,19 @@ import { fadeAnimation } from "./animations";
   styleUrls: ["./app.component.scss"],
   animations: [fadeAnimation]
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(private router: Router) {
+    const navEndEvents$ = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    // Google Analytics
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-108857734-1', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
+}
